c0bbaf7ed5f454363c989081d692b6dbdea82d19
Hossam777 <31036061+Hossam777@users.noreply.github.com>
2019-04-14 09:12:27 +0200

    access tokens
    access token configration added

--- app/User.php
+++ app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
     protected $table = 'users';
     protected $fillable = [
         'u_mail', 'username', 'password','f_name','l_name','age','gender','f_o_i_1','f_o_i_2','f_o_i_3','f_o_i_4','f_o_i_5',
